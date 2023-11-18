6139cedda51ede31b346010598930eebb18e7720
Joanna Puciłowska <jpucilowska@tlen.pl>
2023-11-18 13:56:41 +0100

    initial commit

--- backend/app/src/main/java/app/example/app/AppApplication.java
+++ backend/app/src/main/java/app/example/app/AppApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package app.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AppApplication.class, args);
 	}
 
 }
