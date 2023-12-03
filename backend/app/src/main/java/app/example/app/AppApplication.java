2d85ceda06ebe557a197d25840c2f8587f3a6353
Joanna Puciłowska <jpucilowska@tlen.pl>
2023-12-03 15:24:58 +0100

    add .gitignore

--- backend/app/src/main/java/app/example/app/AppApplication.java
+++ backend/app/src/main/java/app/example/app/AppApplication.java
@@ -1,9 +1,14 @@
 package app.example.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class AppApplication {
 
 	public static void main(String[] args) {
