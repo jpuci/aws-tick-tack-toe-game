d371e80cf5b8a2c4ef8f7371272b22d8e2d51e63
Joanna Puciłowska <jpucilowska@tlen.pl>
2024-01-02 21:30:38 +0100

    mess with docker

--- backend/app/src/test/java/app/example/app/AppApplicationTests.java
+++ backend/app/src/test/java/app/example/app/AppApplicationTests.java
@@ -3,7 +3,7 @@ package app.example.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = app.example.app.AppApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class AppApplicationTests {
 
 	@Test
